a1776622ac3b030ce34f6121a21df548bed6b3eb
喻汉生 <hanshengyu@dadi01.com>
2019-11-08 11:38:26 +0800

    Initial commit

---
+++ src/main/java/com/dadi01/jenkinsdemo1/JenkinsDemo1Application.java
@@ -0,0 +1,13 @@
+package com.dadi01.jenkinsdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDemo1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/dadi01/jenkinsdemo1/JenkinsDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dadi01.jenkinsdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
